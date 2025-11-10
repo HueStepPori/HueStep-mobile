a857a45d368bfa6180cac687164672d278944fd2
haein45 <118984872+haein45@users.noreply.github.com>
2025-11-11 01:52:24 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// GitHub Pages(프로덕션)일 때만 /HueStep/ 사용
 const isProd = process.env.GITHUB_ACTIONS === 'true' || process.env.NODE_ENV === 'production'
 
 export default defineConfig({
   plugins: [react()],
-  base: isProd ? '/HueStep/' : '/',   // ← 저장소명이 HueStep 이므로 여기만 교체
+  base: isProd ? '/HueStep/' : '/',
 })
